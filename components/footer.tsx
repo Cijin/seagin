b40dc7032d3dd1854982b1bcde85d042b9cb32c9
Seagin <cijininurhood@gmail.com>
2024-01-13 11:51:40 +0700

    add subscription for email

---
+++ components/footer.tsx
@@ -0,0 +1,151 @@
+import React, { ChangeEvent, FormEvent } from 'react'
+import { kv } from "@vercel/kv";
+
+const navigation = {
+  social: [
+    {
+      name: 'GitHub',
+      href: 'https://github.com/Cijin',
+      icon: (props:any) => (
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+          <path
+            fillRule="evenodd"
+            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
+    },
+    {
+      name: 'X',
+      href: 'https://twitter.com/cijincherian',
+      icon: (props:any) => (
+        <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+          <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
+        </svg>
+      ),
+    },
+  ],
+}
+
+export default function Footer() {
+   const [email, setEmail] = React.useState('');
+    const [message, setMessage] = React.useState('');
+  const [isError, setIsError] = React.useState(false);
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    try {
+      await kv.set(email, 'subscribed');
+      setMessage('Thank you for subscribing!');
+      setIsError(false);
+    } catch (error) {
+      console.error(error)
+      setMessage('An error occurred. Please try again later.');
+      setIsError(true); 
+    }
+    
+    // Hide the message after 3 seconds
+    setTimeout(() => {
+      setMessage('');
+    }, 3000);
+  }
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  return (
+    <footer className="bg-zinc-950" aria-labelledby="footer-heading">
+      <h2 id="footer-heading" className="sr-only">
+        Footer
+      </h2>
+      <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-12 lg:px-8 lg:pt-16">
+        <div className="lg:flex lg:items-center lg:justify-between">
+          <div>
+            <h3 className="text-sm font-semibold leading-6 text-white">Subscribe to my newsletter</h3>
+            <p className="mt-2 text-sm leading-6 text-gray-300">
+              The latest news, articles, and resources, sent to your inbox weekly.
+            </p>
+          </div>
+          <form onSubmit={handleSubmit} className="mt-6 sm:flex sm:max-w-md lg:mt-0">
+            <label htmlFor="email-address" className="sr-only">
+              Email address
+            </label>
+            <input
+              type="email"
+              name="email-address"
+              id="email-address"
+              autoComplete="email"
+              value={email}
+              onChange={handleChange}
+              required
+              className="w-full min-w-0 appearance-none rounded-md border-0 bg-white/5 px-3 py-1.5 text-base text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:w-56 sm:text-sm sm:leading-6"
+              placeholder="Enter your email"
+            />
+            <div className="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
+              <button
+                className="flex w-full items-center justify-center rounded-md bg-blue-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-400"
+              >
+                Subscribe
+              </button>
+            </div>
+          </form>
+          {message && (
+            <Alert message={message} isError={isError} />
+          )}
+        </div>
+        <div className="mt-8 border-t border-white/10 pt-8 md:flex md:items-center md:justify-between">
+          <div className="flex space-x-6 md:order-2">
+            {navigation.social.map((item) => (
+              <a key={item.name} href={item.href} className="text-gray-500 hover:text-gray-400">
+                <span className="sr-only">{item.name}</span>
+                <item.icon className="h-6 w-6" aria-hidden="true" />
+              </a>
+            ))}
+          </div>
+          <p className="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">
+            &copy; 2024 Seagin, Inc. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+function Alert({ message, isError}: { message: string ,isError: boolean}) {
+  return (
+    <>
+      {isError ?
+        <div className="rounded-md bg-red-100 p-4">
+          <div className="flex">
+            <div className="ml-3">
+              <h3 className="text-sm font-medium text-red-800">Error Subsribing</h3>
+              <div className="mt-2 text-sm text-yellow-red">
+                <p>
+                  {message}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        :
+        <div className="rounded-md bg-red-100 p-4">
+          <div className="flex">
+            <div className="ml-3">
+              <h3 className="text-sm font-medium text-red-800">Success</h3>
+              <div className="mt-2 text-sm text-red-700">
+                <p>
+                  {message}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      }
+    </>
+  )
+}
+
+
