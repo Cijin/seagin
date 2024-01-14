09058872a6985857809123fb880298873eb40e22
Seagin <cijininurhood@gmail.com>
2024-01-14 11:33:23 +0700

    add mastodon

--- components/footer.tsx
+++ components/footer.tsx
@@ -16,6 +16,13 @@ const navigation = {
       ),
     },
     {
+      name: 'Mastodon',
+      href: 'https://mastodon.social/@seagin',
+      icon: (props:any) => (
+        <svg fill='currentColor' {...props} xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M433 179.1c0-97.2-63.7-125.7-63.7-125.7-62.5-28.7-228.6-28.4-290.5 0 0 0-63.7 28.5-63.7 125.7 0 115.7-6.6 259.4 105.6 289.1 40.5 10.7 75.3 13 103.3 11.4 50.8-2.8 79.3-18.1 79.3-18.1l-1.7-36.9s-36.3 11.4-77.1 10.1c-40.4-1.4-83-4.4-89.6-54a102.5 102.5 0 0 1 -.9-13.9c85.6 20.9 158.7 9.1 178.8 6.7 56.1-6.7 105-41.3 111.2-72.9 9.8-49.8 9-121.5 9-121.5zm-75.1 125.2h-46.6v-114.2c0-49.7-64-51.6-64 6.9v62.5h-46.3V197c0-58.5-64-56.6-64-6.9v114.2H90.2c0-122.1-5.2-147.9 18.4-175 25.9-28.9 79.8-30.8 103.8 6.1l11.6 19.5 11.6-19.5c24.1-37.1 78.1-34.8 103.8-6.1 23.7 27.3 18.4 53 18.4 175z"/></svg>
+      ),
+    },
+    {
       name: 'X',
       href: 'https://twitter.com/cijincherian',
       icon: (props:any) => (
@@ -106,7 +113,7 @@ export default function Footer() {
         <div className="mt-8 border-t border-white/10 pt-8 md:flex md:items-center md:justify-between">
           <div className="flex space-x-6 md:order-2">
             {navigation.social.map((item) => (
-              <a key={item.name} href={item.href} className="text-gray-500 hover:text-gray-400">
+              <a rel='me' key={item.name} href={item.href} className="text-gray-500 hover:text-gray-400">
                 <span className="sr-only">{item.name}</span>
                 <item.icon className="h-6 w-6" aria-hidden="true" />
               </a>
